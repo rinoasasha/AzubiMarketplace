be42a19dcf4ead563d1faf1001c8702935d96e95
OYK1FE <oyk1fe@bosch.com>
2026-05-06 10:14:24 +0200

    some

---
+++ backend/Migrations/20260506071937_init_1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class init_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "AspNetUsers",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "AspNetUsers",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TrainingOccupation",
+                table: "AspNetUsers",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<short>(
+                name: "TrainingStartYear",
+                table: "AspNetUsers",
+                type: "year",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TrainingOccupation",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TrainingStartYear",
+                table: "AspNetUsers");
+        }
+    }
+}
